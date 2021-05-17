9a28bcaad3f98567be7a74b0312a36b5d9dd9c5b
Phung Nguyen <phungnnl@dgroup.co>
2021-05-17 15:37:07 +0700

    ✨ feat: rework page

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+console.log(styles);
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Long Phụng</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Pattaya&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <main className={styles.main}>
+        <div className={styles.titleWrapper}>
+          <h1>Long Phụng</h1>
+          <div className={styles.imageWrapper}>
+            <img src="/dragon.png" alt="ok-porcupine" />
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
